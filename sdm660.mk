210e4a5d14aa5af39ffeca8e443818e1fcf3d6c9
Vivekachooz <vivekachooz@gmail.com>
2024-01-16 12:42:31 +0530

    sdm660-common: Decommonize biometrics service
    Change-Id: Id56edf0b769a33eeaeb7236ba76f1fc4b1d06d36
    Signed-off-by: Tiktodz <dotkit@electrowizard.me>

--- sdm660.mk
+++ sdm660.mk
@@ -152,8 +152,6 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1 \
-    android.hardware.biometrics.fingerprint@2.1.vendor \
     liblzma.vendor
 
 # FM
