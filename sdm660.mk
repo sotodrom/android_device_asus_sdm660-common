b7111e23e3b4c60387f770d8d571977a38f2038f
SonicBSV <sonicbsv@gmail.com>
2024-05-25 19:25:27 +0300

    sdm660-common: add android.hardware.bluetooth@1.1
    Change-Id: I685118efbfce49a3d32fd18c7e5790f03bb1bd81
    Signed-off-by: Tiktodz <dotkit@electrowizard.me>

--- sdm660.mk
+++ sdm660.mk
@@ -65,6 +65,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio-impl \
     android.hardware.bluetooth@1.0.vendor \
+    android.hardware.bluetooth@1.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
