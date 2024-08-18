18eef1c2d5e754a356ccb7c0712f2ec70ec449df
Cosmin Tanislav <demonsingur@gmail.com>
2024-08-18 09:52:31 +0300

    sdm660-common: skip dependencies which aren't defined in .bp
    Change-Id: I01a653661856495788c05b720c0577dac44998a4

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -40,6 +40,9 @@ function lib_to_package_fixup_vendor_variants() {
         vendor.qti.hardware.fm@1.0)
             echo "$1-vendor"
             ;;
+        libOmxCore | \
+            libril | \
+            libwpa_client) ;;
         *)
             return 1
             ;;
