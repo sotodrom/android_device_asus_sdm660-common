d028e4a3f320f6b22299f4bcebd5d76822fb37cb
Meisie Widuri <dotkit@electrowizard.me>
2025-01-03 21:54:46 +0700

    sdm660-common: power-libperfmgr: Handle ADPF tag profile with EventNode

--- power-libperfmgr/aidl/PowerSessionManager.cpp
+++ power-libperfmgr/aidl/PowerSessionManager.cpp
@@ -87,8 +87,8 @@ void PowerSessionManager::updateHintMode(const std::string &mode, bool enabled) 
             mDisplayRefreshRate = 60;
         }
     }
-    if (HintManager::GetInstance()->GetAdpfProfile()) {
-        HintManager::GetInstance()->SetAdpfProfile(mode);
+    if (HintManager::GetInstance()->GetAdpfProfileFromDoHint()) {
+        HintManager::GetInstance()->SetAdpfProfileFromDoHint(mode);
     }
 }
 
