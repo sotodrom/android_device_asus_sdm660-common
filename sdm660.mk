bdb780c7a9620909778d82c76dc2e233ed0eed89
Meisie Widuri <dotkit@electrowizard.me>
2025-02-23 11:41:54 +0700

    Update sdm660.mk

--- sdm660.mk
+++ sdm660.mk
@@ -554,10 +554,6 @@ PRODUCT_PACKAGES += \
     init.target.rc \
     ueventd.qcom.rc
 
-# Screen density
-PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG ?= xhdpi
-
 # Seccomp
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
