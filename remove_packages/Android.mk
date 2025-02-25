4e695c4d1359715870bb410963f82ef93c00942c
texascake <budalblimbing@gmail.com>
2025-02-25 15:46:27 +0700

    sdm660-common: Update removepackages

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,40 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Camera2 AudioFX Maps Gmail
+LOCAL_OVERRIDES_PACKAGES := \
+	AudioFX \
+	arcore \
+	AICorePrebuilt \
+	AiWallpapers \
+	AndroidAutoStubPrebuilt \
+	Camera2 \
+	Calendar2 \
+	Calendar \
+	DevicePersonalizationPrebuiltPixel2020 \
+	DeviceAsWebcam \
+	DuckDuckGo \
+	Etar \
+	Flash \
+	GoogleTTS \
+	Gallery2 \
+	Gmail \
+	Jellyfish \
+	Jelly \
+	Maps \
+	Photos \
+	PixelThemesStub \
+	PixelThemesStub2022_and_newer \
+	PixelWallpapers2023 \
+	PixelLiveWallpaperPrebuilt \
+	Recorder \
+	SnapCamera \
+	SafetyHubPrebuilt \
+	Seedvault \
+	SimpleGallery \
+	TurboPrebuilt \
+	WellbeingPrebuilt \
+	YouTube
+
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
