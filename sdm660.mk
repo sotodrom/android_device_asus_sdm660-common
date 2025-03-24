8dec944e57ad29e9f1bd81ff8e2718fc162e8483
Meisie Widuri <dotkit@electrowizard.me>
2025-03-24 21:49:46 +0700

    sdm660-common: Bringup for V-qpr2

--- sdm660.mk
+++ sdm660.mk
@@ -128,11 +128,6 @@ PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
 PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := everything
 USE_DEX2OAT_DEBUG := false
 
-# Use a profile based boot image for this device. Note that this is currently a
-# generic profile and not Android Go optimized.
-PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
-PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
-
 # Disable Scudo outside of eng builds to save RAM.
 PRODUCT_DISABLE_SCUDO := true
 
@@ -377,7 +372,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.secure_element@1.2.vendor \
     com.android.nfc_extras \
-    NfcNci \
     SecureElement \
     Tag
 
