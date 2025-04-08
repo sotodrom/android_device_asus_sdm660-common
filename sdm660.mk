851f6a03292a61c4744c12ffa87c84d3899a363d
Meisie Widuri <dotkit@electrowizard.me>
2025-04-08 14:17:24 +0700

    sdm660-common: Update profile based boot image path
    As per Baklava

--- sdm660.mk
+++ sdm660.mk
@@ -131,7 +131,7 @@ USE_DEX2OAT_DEBUG := false
 # Use a profile based boot image for this device. Note that this is currently a
 # generic profile and not Android Go optimized.
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
-PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/boot/boot-image-profile.txt
 
 # Disable Scudo outside of eng builds to save RAM.
 PRODUCT_DISABLE_SCUDO := true
