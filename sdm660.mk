f18fb99aa8cbd5b8118e69d8c26848574fcec86b
Bruno Martins <bgcngm@gmail.com>
2025-08-21 23:54:56 +0700

    sdm660-common: Switch to Touch AIDL
    * Original:
    https://github.com/LineageOS/android_hardware_oneplus/commit/9b0408ca4c6aea8b164bb99fa3f89177ed17f359
    
    Signed-off-by: rsuntk <rsuntk@yukiprjkt.my.id>
    Signed-off-by: Tiktodz <dotkit@electrowizard.me>

--- sdm660.mk
+++ sdm660.mk
@@ -532,7 +532,7 @@ PRODUCT_PACKAGES += \
 
 # Touch
 PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.asus_sdm660
+    vendor.lineage.touch-service.asus_sdm660
 
 # USB
 PRODUCT_PACKAGES += \
