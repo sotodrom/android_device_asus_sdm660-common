d4a311c9b0fb7a57030e241a0196022ba4c5fd55
Debayan Kar <kardebayan3@gmail.com>
2026-03-29 06:36:01 +0000

    sdm660-common: Fix vendor.qti.hardware.mwqemadapter@1.0 ELF checks
    Change-Id: Id331521740541ef39653edc6587b664daba022d5

--- extract-files.py
+++ extract-files.py
@@ -35,6 +35,7 @@ lib_fixups: lib_fixups_user_type = {
     (
         'com.qualcomm.qti.dpm.api@1.0',
         'vendor.qti.hardware.fm@1.0',
+        'vendor.qti.hardware.mwqemadapter@1.0',
         'vendor.qti.imsrtpservice@3.0',
     ): lib_fixup_vendor_suffix,
 }
