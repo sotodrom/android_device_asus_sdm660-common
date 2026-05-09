3ee84c947fd1b1ce905709ea07b4a8d859d10496
Tiktodz <dotkit@electrowizard.me>
2026-05-09 02:41:10 +0000

    Revert "sdm660-common: Retrofit dynamic partitions"
    This reverts commit ab8becd690463b6502c5806129eeeec95681fb76.

--- sdm660.mk
+++ sdm660.mk
@@ -230,8 +230,6 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 # Partitions
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
-
  PRODUCT_PACKAGES += \
      vendor_bt_firmware_mountpoint \
      vendor_dsp_mountpoint \
